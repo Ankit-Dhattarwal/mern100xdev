612874d33de042ec15f792ad6c38d1975cfd07a5
Ankit-Dhattarwal <ankitdhatterwal09@gmail.com>
2024-05-25 02:50:46 +0530

    useRecoilStateLodable , useRecoilValueLoadable this also say use to handle the exception like if not hite backend and we can more method too use like Suspense , ErrorBoundary

--- week7/week7.4/selector-family/Async-Recoil-part2/src/App.jsx
+++ week7/week7.4/selector-family/Async-Recoil-part2/src/App.jsx
@@ -4,6 +4,7 @@ import {
   useRecoilState,
   useRecoilValue,
   useSetRecoilState,
+  useRecoilStateLoadable,
 } from "recoil";
 import { todosAtomFamily } from "./components/atoms/atoms";
 import { useEffect } from "react";
@@ -11,7 +12,7 @@ import { useEffect } from "react";
 function App() {
   return (
     <RecoilRoot>
-      <UpdateComponent />
+      {/* <UpdateComponent /> */}
       <Todo id={1} />
       <Todo id={2} />
       <Todo id={2} />
@@ -34,15 +35,27 @@ function UpdateComponent() {
 }
 
 function Todo({ id }) {
-  const currentTodo = useRecoilValue(todosAtomFamily(id));
-
-  return (
-    <>
-      {currentTodo.title}
-      {currentTodo.description}
-      <br></br>
-    </>
+  const [currentTodo, setCurrentTodo] = useRecoilStateLoadable(
+    todosAtomFamily(id)
   );
+
+  if (currentTodo.state === "loading") {
+    return <div>loading...</div>;
+  } else if (currentTodo.state === "hasValue") {
+    return (
+      <>
+        {currentTodo.contents.title}
+        {currentTodo.contents.description}
+        <br></br>
+      </>
+    );
+  } else if (currentTodo.state === "hasError") {
+    return (
+      <div>
+        <h3>Error while getting data from backend.</h3>
+      </div>
+    );
+  }
 }
 
 export default App;
--- week7/week7.4/selector-family/Async-Recoil-part2/src/components/atoms/atoms.js
+++ week7/week7.4/selector-family/Async-Recoil-part2/src/components/atoms/atoms.js
@@ -10,6 +10,7 @@ export const todosAtomFamily = atomFamily({
     get:
       (id) =>
       async ({ get }) => {
+        await new Promise((r) => setTimeout(r, 5000));
         const res = await axios.get(
           `https://sum-server.100xdevs.com/todo?id=${id}`
         );
